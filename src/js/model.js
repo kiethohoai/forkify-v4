24a109b0ca869c555509eec8581a057afaed5b64
David Ho <kietho.060193@gmail.com>
2024-11-14 14:59:09 +0700

    010 Helpers and Configuration Files

--- src/js/model.js
+++ src/js/model.js
@@ -1,3 +1,6 @@
+import { getJSON } from './helpers.js';
+import { API_URL } from './config.js';
+
 // todo state
 export const state = {
   recipe: {},
@@ -5,21 +8,23 @@ export const state = {
 
 // todo loadRecipe
 export const loadRecipe = async function (id) {
-  /* Fetch & get data */
-  const res = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-  const data = await res.json();
-  if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+  try {
+    /* Fetch & get data */
+    const data = await getJSON(`${API_URL}/${id}`);
 
-  /* Format data */
-  let { recipe } = data.data;
-  state.recipe = {
-    id: recipe.id,
-    title: recipe.title,
-    image: recipe.image_url,
-    ingredients: recipe.ingredients,
-    publisher: recipe.publisher,
-    servings: recipe.servings,
-    sourceUrl: recipe.source_url,
-    cookingTime: recipe.cooking_time,
-  };
+    /* Format data */
+    let { recipe } = data.data;
+    state.recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      image: recipe.image_url,
+      ingredients: recipe.ingredients,
+      publisher: recipe.publisher,
+      servings: recipe.servings,
+      sourceUrl: recipe.source_url,
+      cookingTime: recipe.cooking_time,
+    };
+  } catch (error) {
+    throw error;
+  }
 };
