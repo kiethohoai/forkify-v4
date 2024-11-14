7741af9dbe0b9f84a9fdcc6954d9aabf143ab5dc
David Ho <kietho.060193@gmail.com>
2024-11-14 22:11:03 +0700

    015 Implementing Pagination - Part 1

--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import { getJSON } from './helpers.js';
-import { API_URL } from './config.js';
+import { API_URL, RES_PER_PAGE } from './config.js';
 
 // todo state
 export const state = {
@@ -7,6 +7,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    resultsPerPage: RES_PER_PAGE,
+    page: 1,
   },
 };
 
@@ -53,4 +55,11 @@ export const loadSearchResults = async function (query) {
     throw error;
   }
 };
-loadSearchResults();
+
+// todo getSearchResultsPage
+export const getSearchResultsPage = function (page = state.search.page) {
+  state.search.page = page;
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+  return state.search.results.slice(start, end);
+};
